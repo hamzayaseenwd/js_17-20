841bc4a15a8b8755bedc411dffd3d79bed784cb9
hamzayaseenwd <hamzayaseen836@gmail.com>
2023-09-19 17:05:34 +0500

    js update

---
+++ app.js
@@ -0,0 +1,78 @@
+// Q3
+
+
+for (var i = 1; i <= 10; i++ ) {
+  console.log(i);
+  document.write(i + "<br>");
+}
+
+
+
+
+// Q4
+
+
+// var userInput = +prompt("Enter a number to generate a multiplication table");
+
+// var tableLength = +prompt("Entetr table length");
+
+// for (var i = 1; i <= tableLength; i++) {
+//   document.write(userInput + "x" + i + "=" + userInput * i + "<br>");
+// }
+
+
+// Q5
+
+
+var fruits = ["apple", "banana", "mango", "orange", "strawberry"];
+
+for (var i = 0; i < fruits.length; i++) {
+  console.log(fruits[i]);
+  document.write("Element at Index " + i + " is " + fruits[i] + "<br>")
+}
+
+
+// Q6a
+
+// for (var i = 1; i <=15; i++) {
+//     console.log(i)
+    
+// }
+
+// Q6b
+
+
+// for (var i = 15; i >=1; i--) {
+//     console.log(i)
+       
+// }
+
+// Q6c
+
+
+// for (var i = 0; i =20; i + 2) {
+//     console.log(i)
+
+// }
+
+// Q6d
+
+
+// for (var i = 1; i =20; i + 2 ) {
+//     console.log(i)
+
+// }
+
+// Q6e
+
+
+// for (var i = 0; i <=20; i + 2 ) {
+//     console.log(i +"k",)
+
+//}
+
+// Q10
+
+for (var i = 5; i =100; i) {
+    console.log(i * 2);
+}
